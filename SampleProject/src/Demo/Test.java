276ea3ff9da52127367c2c73a7b81386d69987c2
unknown <yashsbhagat99@gmail.com>
2023-07-18 11:21:48 +0530

    commiting

---
+++ SampleProject/src/Demo/Test.java
@@ -0,0 +1,22 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class Test {
+
+	
+	public static void main(String[] args) {
+		
+		int a,b,sum;
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.println("please enter the first number");
+		a=sc.nextInt();
+		System.out.println("please enter the Second number");
+		b=sc.nextInt();
+		sum=a+b;
+		System.out.println("sum of given number "+sum);
+		
+		
+	}
+}
